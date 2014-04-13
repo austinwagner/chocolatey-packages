0b5ffc5bf87423ef63b042acb1512a6cbc8b90a2
Austin Wagner <austinwagner@gmail.com>
2014-04-12 22:20:02 -0400

    Add VS2013 VSIX Installer Template

---
+++ _templates/vsix2013/tools/chocolateyInstall.ps1
@@ -0,0 +1,48 @@
+﻿$packageName = '__NAME__'
+$url = '__REPLACE__'
+
+function Invoke-BatchFile($file) {
+    $cmd = "`"$file`" & set"
+    cmd /c $cmd | Foreach-Object {
+        $p, $v = $_.split('=')
+        Set-Item -path env:$p -value $v
+    }
+}
+
+function Invoke-VsVars32() {
+    $batchFile = Join-Path $env:VS120COMNTOOLS 'vsvars32.bat'
+    Invoke-Batchfile `"$batchFile`"
+}
+
+function Get-TempWebFile($url, $filename) {
+    $path = Join-Path ([IO.Path]::GetTempPath()) $filename
+
+    if (Test-Path $path) { 
+        Remove-Item -Force $path 
+    }
+
+    Get-ChocolateyWebFile $packageName $path $url
+    return $path
+}
+
+function Install-VsixSilently($url, $name) {
+    $extension = Get-TempWebFile $url $name
+
+    try {
+        Start-Process -FilePath 'VSIXInstaller.exe' -ArgumentList "/q $extension" -Wait
+    } finally {
+        if (Test-Path $extension) {
+            Remove-Item -Force $extension
+        }
+    }
+}
+
+try {
+    Invoke-VsVars32
+    Install-VsixSilently $url ($packageName + '.vsix')
+
+    Write-ChocolateySuccess $packageName
+} catch {
+    Write-ChocolateyFailure $packageName "$($_.Exception.Message)"
+    throw
+}
---
+++ _templates/vsix2013/tools/chocolateyUninstall.ps1
@@ -0,0 +1,28 @@
+﻿$vsixId = '__REPLACE__'
+
+function Invoke-BatchFile ($file) {
+    $cmd = "`"$file`" & set"
+    cmd /c $cmd | Foreach-Object {
+        $p, $v = $_.split('=')
+        Set-Item -path env:$p -value $v
+    }
+}
+
+function Invoke-VsVars32() {
+    $batchFile = Join-Path $env:VS120COMNTOOLS 'vsvars32.bat'
+    Invoke-Batchfile `"$batchFile`"
+}
+
+function Uninstall-VsixSilently($id) {
+    Start-Process -FilePath 'VSIXInstaller.exe' -ArgumentList "/q /u:$id" -Wait
+}
+
+try {
+    Invoke-VsVars32
+    Uninstall-VsixSilently $vsixId
+
+    Write-ChocolateySuccess $packageName
+} catch {
+    Write-ChocolateyFailure $packageName "$($_.Exception.Message)"
+    throw
+}
