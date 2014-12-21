4167a7fab0f27011e746f078ed148d27c0e2d59d
Austin Wagner <austinwagner@gmail.com>
2014-12-21 15:59:13 -0500

    Commit scripts to generate version hash for URL

---
+++ ps-fileassociation/scripts/Register-FileAssociation.ps1
@@ -0,0 +1,89 @@
+﻿<#
+.SYNOPSIS
+    Creates a file type association based off file extension.
+
+.DESCRIPTION
+    Registers the given extension into the Windows Registry to invoke the provided command. The command should take the form of '"C:\Path\To\Program.exe" "%1"'.
+    %1 will be substituted with the file path by Windows.
+
+.PARAMETER Scope
+    Selects the scope of the registration. Must be either 'LocalMachine' or 'User'.
+
+.PARAMETER AppName
+    The name of the application this extension will be registered to. Will be combined with Extension to create a unique name in the registry.
+
+.PARAMETER Extension
+    The extension to register (without the dot).
+
+.PARAMETER Command
+    The command that will be executed when a file of the given type is opened from Explorer. %1 in this string will be replaced with the file path.
+    Ensure this parameter is properly quoted.
+
+.PARAMETER Icon
+    Path to the icon that represents this type. If the icon is inside a DLL or EXE, the path can be followed by a comma and a number indicating the index of the icon resource.
+
+.PARAMETER Force
+    Force creation of the file association even if 'AppName.Command' already exists.
+#>
+param (
+    [ValidateSet('User', 'LocalMachine', IgnoreCase = $true)]
+    [Parameter(Mandatory=$true)]
+    [string]
+    $Scope,
+
+    [Parameter(Mandatory=$true)]
+    [ValidateNotNullOrEmpty]
+    [string]
+    $AppName,
+
+    [Parameter(Mandatory=$true)]
+    [ValidateNotNullOrEmpty]
+    [string]
+    $Extension,
+    
+    [Parameter(Mandatory=$true)]
+    [ValidateNotNullOrEmpty]
+    [string]
+    $Command,
+
+    [string]
+    $Description,
+
+    [string]
+    $Icon,
+
+    [switch]
+    $Force
+)
+
+$hive = switch ($Scope)
+        {
+            'User' { 'HKCU' }
+            'LocalMachine' { 'HKLM' }
+        }
+
+$classesRoot = "${hive}:\Software\Classes"
+$fullName = "$AppName.$Extension"
+
+Set-Location $classesRoot | Out-Null
+New-Item -Name $fullName -Force:$Force.IsPresent | Out-Null
+
+Set-Location $fullName | Out-Null
+
+if ($Description)
+    { Set-Item -Path . -Value $Description | Out-Null }
+
+if ($Icon)
+{
+    New-Item -Name 'Default Icon' | Out-Null
+    Set-Location 'Default Icon' | Out-Null
+    Set-Item -Path . -Value "$Icon" | Out-Null
+    Set-Location .. | Out-Null
+}
+
+New-Item -Name 'shell\open\command' | Out-Null
+Set-Location 'shell\open\command' | Out-Null
+Set-Item -Path . -Value "$Command" | Out-Null
+
+New-Item -Name "$classesRoot\.$Extension" -ErrorAction Ignore | Out-Null
+Set-Item -Path "$classesRoot\.$Extension" -Value $fullName | Out-Null
---
+++ ps-fileassociation/scripts/Unregister-FileAssociation.ps1
@@ -0,0 +1,90 @@
+﻿<#
+.SYNOPSIS
+    Removes a file type association based off file extension.
+
+.DESCRIPTION
+    Unegisters the given extension's association and deletes the entry created by Register-FileAssociation. 
+    Will not unregister if the association does not match the one created by Register-FileAssociation unless the Force switch is given.
+    The complementary Register-FileAssoctation entry will always be deleted.
+
+.PARAMETER Scope
+    Selects the scope of the registration to remove. Must be either 'LocalMachine' or 'User'.
+
+.PARAMETER AppName
+    The name of the application this extension will be unregistered from. Will be combined with Extension to create a unique name in the registry.
+
+.PARAMETER Extension
+    The extension to unregister (without the dot).
+
+.PARAMETER FullName
+    The name of the entry in the registry if it does not follow the ${AppName}.${Extension} format used by Register-FileAssociation.
+
+.PARAMETER Force
+    Force any registration for the given extension to be removed even if it was not created by the matching Register-FileAssociation.
+#>
+param (
+    [ValidateSet('User', 'LocalMachine', IgnoreCase = $true)]
+    [Parameter(Mandatory=$true)]
+    [string]
+    $Scope,
+
+    [Parameter(Mandatory=$true, ParameterSetName='MatchRegisterParams')]
+    [ValidateNotNullOrEmpty()]
+    [string]
+    $AppName,
+
+    [Parameter(Mandatory=$true, ParameterSetName='MatchRegisterParams')]
+    [Parameter(Mandatory=$true, ParameterSetName='FullName')]
+    [ValidateNotNullOrEmpty()]
+    [string]
+    $Extension,
+
+    [Parameter(Mandatory=$true, ParameterSetName='FullName')]
+    [ValidateNotNullOrEmpty()]
+    [string]
+    $FullName,
+
+    [switch]
+    $Force
+)
+
+$hive = switch ($Scope)
+        {
+            'User' { 'HKCU' }
+            'LocalMachine' { 'HKLM' }
+        }
+
+$classesRoot = "${hive}:\Software\Classes"
+
+if ($PSCmdlet.ParameterSetName -eq 'MatchRegisterParams')
+    { $FullName = "$AppName.$Extension" }
+
+$extensionPath = "$classesRoot\.$Extension"
+
+$extensionKeyValue = Get-Item $extensionPath -ErrorAction Ignore | Where-Object { $_ -ne $null } | ForEach-Object { $_.GetValue('') }
+
+if (-not $Force -and $extensionKeyValue -ine $fullName)
+{
+    Write-Warning ".$Extension is not associated with $AppName. Not removing." 
+}
+else
+{
+    $removeFailed = $false
+
+    if ((Get-Item $extensionPath).ValueCount -gt 1)
+    {
+        $removeFailed = $true
+    }
+    else
+    {
+        Remove-Item $extensionPath -ErrorAction Ignore -ErrorVariable removeFailed
+    }
+
+    if ($removeFailed)
+    {
+        Write-Verbose "$extensionPath has other subkeys or values that were not created by Register-FileAssociation. Changing default value instead of removing key."
+        Set-Item -Path $extensionPath -Value '' | Out-Null
+    }
+}
+
+Remove-Item "$classesRoot\$fullName" -Recurse
