9799403a481e309ed3c41f71fc96a738667716bb
Austin Wagner <austinwagner@gmail.com>
2014-12-21 14:58:50 -0500

    Add sha1 hash for downloads

--- Icaros/Icaros.install/tools/chocolateyInstall.ps1
+++ Icaros/Icaros.install/tools/chocolateyInstall.ps1
@@ -1,7 +1,8 @@
 ﻿$packageName = 'Icaros.install'
 $version = '2.2.6'
 $installerType = 'EXE'
+$sha1 = '2a9b4916aff45c72c068e1008c40edf395457980'
 $url = "http://www.austinwagner.net/files/Icaros_v$version.exe"
 $silentArgs = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
 
-Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url"
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" -checksum "$sha1" -checksumType 'sha1'
