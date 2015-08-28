b937fe4fdd6d4bc243ebc8b8c1c86c4d504c2a2e
Austin Wagner <austinwagner@gmail.com>
2015-08-28 12:09:31 -0400

    Remove current Icaros files

--- Icaros/Icaros.install/tools/chocolateyUninstall.ps1
+++
@@ -1,7 +0,0 @@
-﻿$packageName = 'Icaros.install'
-$installerType = 'EXE'
-$silentArgs = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART'
-
-$exe = Join-Path "$env:ProgramFiles" 'Icaros\unins000.exe'
-
-Uninstall-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$exe"
--- Icaros/Icaros/tools/chocolateyInstall.ps1
+++
@@ -1,1 +0,0 @@
-﻿#Install-VirtualPackage 'Icaros.portable' 'Icaros.install'
