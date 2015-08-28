e601ff4dac0b1772efdfda36096d670b89215e28
Austin Wagner <austinwagner@gmail.com>
2015-08-28 14:41:02 -0400

    Fix download script, fix copyright and author

--- icaros/beta/tools/chocolateyInstall.ps1
+++ icaros/beta/tools/chocolateyInstall.ps1
@@ -2,7 +2,15 @@
 $version = '3.0.0_b4'
 $installerType = 'EXE'
 $sha1 = '1f106b486dd294a3995770d21127857797d56d28'
-$url = "http://www.videohelp.com/download/Icaros_v$version.exe"
+$urlBase = "http://www.videohelp.com/download/Icaros_v$version.exe?r="
 $silentArgs = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
 
-Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" -checksum "$sha1" -checksumType 'sha1'
+$downloadUrl = 
+    Invoke-WebRequest -Uri http://www.videohelp.com/software/Icaros -UseBasicParsing |
+    select -ExpandProperty Links |
+    ? { $_ -like '*itemprop="downloadURL"*' } |
+    select -First 1 -ExpandProperty href
+
+$referrer = $downloadUrl -replace '.*\?r=(\w+)','$1'
+
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$urlBase$referrer" -checksum "$sha1" -checksumType 'sha1'
--- icaros/stable/tools/chocolateyInstall.ps1
+++ icaros/stable/tools/chocolateyInstall.ps1
@@ -2,7 +2,15 @@
 $version = '2.3.0'
 $installerType = 'EXE'
 $sha1 = 'c9cc1b488cade186ba28436dd4db5e2508f49559'
-$url = "http://www.videohelp.com/download/Icaros_v$version.exe"
+$urlBase = "http://www.videohelp.com/download/Icaros_v$version.exe?r="
 $silentArgs = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
 
-Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" -checksum "$sha1" -checksumType 'sha1'
+$downloadUrl = 
+    Invoke-WebRequest -Uri http://www.videohelp.com/software/Icaros -UseBasicParsing |
+    select -ExpandProperty Links |
+    ? { $_ -like '*itemprop="downloadURL"*' } |
+    select -First 1 -ExpandProperty href
+
+$referrer = $downloadUrl -replace '.*\?r=(\w+)','$1'
+
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$urlBase$referrer" -checksum "$sha1" -checksumType 'sha1'
