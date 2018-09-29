bbbb0f3aaad256dd40565f3281ce9510b53eb29a
Austin Wagner <austinwagner@gmail.com>
2018-09-29 09:05:34 -0400

    Update to Icaros v3.1.0 stable Add some recommended nuspec fields

--- icaros/stable/tools/chocolateyInstall.ps1
+++ icaros/stable/tools/chocolateyInstall.ps1
@@ -1,13 +1,13 @@
 ﻿$packageName = 'icaros'
-$version = '3.0.3'
+$version = '3.1.0'
 $installerType = 'EXE'
-$sha512 = '9FDDFF658FE8981C6AB86E0C85927B49C11F0E0BCC5C94123637DC31179C5BA394C2932E55AF1D5AD7BE207BD5637051675F6B58C51F3AD7FC8A08A4E8976404'
+$sha512 = '19F7691E7B99F8A852AB130CC3822B5AF5B3281E101A36B00C2470AA72FE18867EFB8BBCA114AEF9260A24128181CD35372F21BD67CFB45D713907C3E854A747'
 $silentArgs = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
 
 $downloadUrl = 
     Invoke-WebRequest -Uri "http://www.videohelp.com/download/Icaros_v$version.exe" -UseBasicParsing |
-    select -ExpandProperty Links |
-    ? { $_ -like "*Icaros_v$version*" } |
-    select -First 1 -ExpandProperty href
+    Select-Object -ExpandProperty Links |
+    Where-Object { $_ -like "*Icaros_v$version*" } |
+    Select-Object -First 1 -ExpandProperty href
 
 Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$downloadUrl" -checksum "$sha512" -checksumType 'sha512'
